86f526c075977db4c22da398a2d72a01297f599b
dubssfy <ponkshesm@gmail.com>
2026-07-25 19:34:48 +0530

    Allow Railway host in Vite preview

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react],
+
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173,
+    allowedHosts: [
+      "customer-website-production-f7db.up.railway.app",
+      "swachham.co.in",
+      "www.swachham.co.in"
+    ]
+  }
 })
