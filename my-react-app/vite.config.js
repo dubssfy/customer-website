45847177cbcc9a12e7423bb77f4adc31f758ef94
dubssfy <ponkshesm@gmail.com>
2026-07-25 19:43:33 +0530

    Vite change

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -7,6 +7,10 @@ export default defineConfig({
   preview: {
     host: "0.0.0.0",
     port: Number(process.env.PORT) || 4173,
-    allowedHosts: true,
+    allowedHosts: [
+      "customer-website-production-f7db.up.railway.app",
+      "swachham.co.in",
+      "www.swachham.co.in",
+    ],
   },
 });
